ed093e4330e9d467cf6c9e190ac19f983f2ef2a0
MuhammadFasya <@mhmdfasya15@gmail.com>
2025-02-05 12:39:39 +0700

    "Revisi 2"

--- Code/index.js
+++ Code/index.js
@@ -26,5 +26,5 @@ app.use("/api/location", locationRoutes);
 app.use("/api/weather", weatherRoutes);
 app.use("/api/email", emailRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server run on  http://localhost:${PORT}`));
